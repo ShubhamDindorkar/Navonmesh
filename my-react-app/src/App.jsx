0eea0ac5bbf5181225ea2dd3e184391d66e8ecde
Shubham Dindorkar <shubhamdindorkar1@gmail.com>
2025-02-23 17:20:01 +0530

    changed the ui

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import Header from './components/Header'
-import Hero from './components/Hero'
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
-
-
-function App() {
+const App = () => {
   return (
-    <div className="min-h-screen w-full bg-black font-syne">
-      <Header />
-      <Hero />
-    </div>
-  )
-}
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
 
-export default App
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
--- my-react-app/src/components/Hero.jsx
+++ my-react-app/src/components/Hero.jsx
@@ -1,51 +1,104 @@
-import React from 'react'
+import { curve, heroBackground, robot } from "../assets";
+import Button from "./Button";
+import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
-    <div className="min-h-[calc(100vh-4rem)] w-full bg-black flex flex-col items-center justify-center relative p-8">
-      {/* Left side - Logo with header background */}
-      <div className="absolute left-10 top-10 w-[300px] h-[200px] relative">
-        <img 
-          src="/assets/header.svg"
-          alt="Header Background" 
-          className="w-full h-full object-contain"
-        />
-        <img 
-          src="/assets/navonmesh1.svg"
-          alt="Navonmesh Logo" 
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[200px]"
-        />
+    <Section
+      className="pt-[12rem] -mt-[5.25rem]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative" ref={parallaxRef}>
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6">
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            <span className="inline-block relative">
+              Brainwave{" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button href="/pricing" white>
+            Get started
+          </Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src={robot}
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
+
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
+              </div>
+            </div>
+
+            <Gradient />
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="hero"
+            />
+          </div>
+
+          <BackgroundCircles />
+        </div>
+
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
 
-      {/* Right side - Logodes */}
-      <div className="absolute right-10 top-10">
-        <img 
-          src="/assets/logodes.svg"
-          alt="Logo Design" 
-          className="w-[200px] h-auto object-contain"
-        />
-      </div>
+      <BottomLine />
+    </Section>
+  );
+};
 
-      {/* Center content - Slogan and Short slogan */}
-      <div className="flex flex-col items-center gap-8">
-        <img 
-          src="/assets/slogan.svg"
-          alt="Slogan" 
-          className="w-[500px] h-auto object-contain"
-        />
-        <img 
-          src="/assets/shortslogan.svg"
-          alt="Short Slogan" 
-          className="w-[400px] h-auto object-contain"
-        />
-      </div>
-
-      {/* Debug information */}
-      <div className="absolute bottom-4 left-4 text-white text-xs">
-        Debug: Images should be loaded from /assets/
-      </div>
-    </div>
-  )
-}
-
-export default Hero 
+export default Hero;
